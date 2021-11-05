7cb32e3a645d75778e234fe69052f42edab1cf56
Igor Artsimovich <artsimovich.igor@gmail.com>
2021-11-05 23:23:39 +0300

    001

---
+++ test calc/test calc/test calc/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  test calc
+//
+//  Created by Igor Artimovich on 5.11.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var dplabel: UILabel!
+    
+    var dptext: String = "0"{
+        didSet {
+        dplabel.text = dptext
+    }
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        dptext = "0"
+        
+    }
+
+    @IBAction func dbt(_ digitButton: UIButton) {
+        updatedptext(with: String(digitButton.tag))
+    }
+    func updatedptext(with newText: String) {
+        if dptext == "0" && newText == "0" {
+            return
+        }
+        if dptext == "0"{
+            dptext = newText
+            return
+        }
+        if dptext.count >= 9 {
+            return
+    }
+    dptext += newText
+}
+}
---
+++ test calc/test calc/test calcTests/test_calcTests.swift
@@ -0,0 +1,33 @@
+//
+//  test_calcTests.swift
+//  test calcTests
+//
+//  Created by Igor Artimovich on 5.11.21.
+//
+
+import XCTest
+@testable import test_calc
+
+class test_calcTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
